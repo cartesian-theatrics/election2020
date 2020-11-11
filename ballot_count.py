6bd3d7bf26697037b90410c9222c53df384956d7
John Collins <john.collins@braincorp.com>
2020-11-11 01:11:21 -0800

    Fix

--- ballot_count.py
+++ ballot_count.py
@@ -1,21 +1,11 @@
-import pandas as pd
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-import plotly.express as px
 import os
-import pprint
-from dateutil import parser
-import requests
 import pickle
 
-
-def collapse_results_by_party(results_by_candidate, candidates):
-    results_by_party = {}
-    for candidate, count in results_by_candidate.items():
-        party = candidates[candidate]['party']
-        results_by_party[party] = results_by_party.get(party, 0) + count
-
-    return results_by_party
+import pandas as pd
+import plotly.graph_objects as go
+import requests
+from dateutil import parser
+from plotly.subplots import make_subplots
 
 
 def plot_state(state_data):
@@ -40,28 +30,7 @@ def plot_state(state_data):
                           line_width=1))
 
 
-def plot_all_states(state_data):
-    xs, ys, colors, text = [], [], [], []
-    for data_point in state_data['counties']:
-        vote_shares = data_point['vote_shares']
-        date = parser.parse(data_point['timestamp'])
-
-        xs.extend([date, date, date])
-        ys.extend([vote_shares['trumpd'], vote_shares['bidenj'],
-                   1 - (vote_shares['trumpd'] + vote_shares['bidenj'])])
-        colors.extend([1, 3, 5])
-        text.extend(["Trump", "Biden", "Other"])
-    return go.Scatter(x=xs,
-                      y=ys,
-                      mode='markers',
-                      text=text,
-                      marker=dict(
-                          color=colors,
-                          colorscale='Viridis',
-                          line_width=1))
-
-
-def country_plots(election_data):
+def plot_all_states(election_data):
     """
     :param election_data:
     """
@@ -81,15 +50,18 @@ def country_plots(election_data):
     return fig
 
 
-states = ['Alaska', 'Alabama', 'Arkansas', 'Arizona', 'California', 'Colorado',
-          'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Iowa', 'Idaho',
-          'Illinois', 'Indiana', 'Kansas', 'Kentucky', 'Louisiana', 'Massachusetts',
-          'Maryland', 'Maine', 'Michigan', 'Minnesota', 'Missouri', 'Mississippi',
-          'Montana', 'North Carolina', 'North Dakota', 'Nebraska', 'New Hampshire',
-          'New Jersey', 'New Mexico', 'Nevada', 'New York', 'Ohio', 'Oklahoma',
-          'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota',
-          'Tennessee', 'Texas', 'Utah', 'Virginia', 'Vermont', 'Washington',
-          'Wisconsin', 'West Virginia', 'Wyoming']
+states = [
+    'Alaska', 'Alabama', 'Arkansas', 'Arizona', 'California', 'Colorado',
+    'Connecticut', 'Delaware', 'Florida', 'Georgia',
+    'Hawaii', 'Iowa', 'Idaho', 'Illinois', 'Indiana', 'Kansas', 'Kentucky',
+    'Louisiana', 'Massachusetts', 'Maryland', 'Maine', 'Michigan',
+    'Minnesota', 'Missouri', 'Mississippi', 'Montana', 'North Carolina',
+    'North Dakota', 'Nebraska', 'New Hampshire', 'New Jersey', 'New Mexico',
+    'Nevada', 'New York', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
+    'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas',
+    'Utah', 'Virginia', 'Vermont', 'Washington', 'Wisconsin',
+    'West Virginia', 'Wyoming',
+]
 
 if os.path.exists("election-assets.pkl"):
     all_results = pickle.load(open("election-assets.pkl", 'rb'))
@@ -98,40 +70,10 @@ else:
     for state in states:
         print('Downloading {}'.format(state))
         formatted_state = state.lower().replace(' ', '-')
-        state_results = requests.get('https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/race-page/{}/president.json'.format(formatted_state)).json()
+        state_results = requests.get(f'https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/race-page/{formatted_state}/president.json').json()
         all_results[formatted_state] = state_results
 
     pickle.dump(all_results, open("election-assets.pkl", 'wb'))
 
 fig = plot_all_states(all_results)
 fig.show()
-
-# records = []
-# for state, state_results in all_results.items():
-#     race = state_results['data']['races'][0]
-# 
-#     for candidate in race['candidates']:
-#         if candidate['party_id'] == 'republican':
-#             candidate['party'] = 'rep'
-#         elif candidate['party_id'] == 'democrat':
-#             candidate['party'] = 'dem'
-#         else:
-#             candidate['party'] = 'trd'
-#     candidates = { candidate['candidate_key']: candidate for candidate in race['candidates'] }
-# 
-#     for data_point in race['timeseries']:
-#         data_point['state']             = state
-#         data_point['expected_votes']    = race['tot_exp_vote']
-#         data_point['trump2016']         = race['trump2016']
-#         data_point['votes2012']         = race['votes2012']
-#         data_point['votes2016']         = race['votes2016']
-# 
-#         vote_shares = collapse_results_by_party(data_point['vote_shares'], candidates)
-#         for party in ['rep', 'dem', 'trd']:
-#             data_point['vote_share_{}'.format(party)] = vote_shares.get(party, 0)
-# 
-#         data_point.pop('vote_shares')
-#         records.append(data_point)
-# 
-# time_series_df = pd.DataFrame.from_records(records)
-# time_series_df.to_csv('nyt_ts.csv', encoding='utf-8')
